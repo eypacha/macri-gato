be3b0b809d19b11667e061db742395fba5ffbae7
Ey Pacha! <contacto@eypacha.com.ar>
2016-11-01 23:03:30 -0300

    version 7, bug fuera...

--- inject.js
+++ inject.js
@@ -1,9 +1,13 @@
-var node,
-    ni = document.createNodeIterator(document.documentElement, NodeFilter.SHOW_TEXT);
+function gatear(){
+    var node,
+        ni = document.createNodeIterator(document.documentElement, NodeFilter.SHOW_TEXT);
 
-while(node = ni.nextNode()) {
-    var value = node.nodeValue
-    if (value.match(/macri/i)) {
-          node.nodeValue = value.replace(/(Mauri(cio)? )?Macri(?! Gato)/gi,'Macri Gato');
+    while(node = ni.nextNode()) {
+        var value = node.nodeValue
+        if (value.match(/macri/i)) {
+              node.nodeValue = value.replace(/(Mauri(cio)? )?Macri(?! Gato)/gi,'Macri Gato');
+        }
     }
 }
+
+gatear();
